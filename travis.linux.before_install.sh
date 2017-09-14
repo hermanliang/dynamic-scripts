1186078f06d8b252486b07f4d071ab00e5b3128d
Herman Liang <hermanliang80@gmail.com>
2017-09-15 00:20:31 +0800

    Update travis.linux.before_install.sh

--- travis.linux.before_install.sh
+++ travis.linux.before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 
 # Install infer (latest version)
 # Checkout Infer
