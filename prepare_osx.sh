b8aa3f63c1b53332a024261b958f1bbd17a941a5
Herman Liang <hermanliang80@gmail.com>
2017-09-15 11:12:44 +0800

    Create prepare_osx.sh

---
+++ prepare_osx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+brew tap caskroom/cask
+brew cask install android-sdk
+eval echo \"y\" | android update sdk --no-ui --force --all --filter extra-google-m2repository || true
+eval echo \"y\" | android update sdk --no-ui --force --all --filter build-tools-26.0.1 || true
+eval echo \"y\" | android update sdk --no-ui --force --all --filter android-26 || true
+export ANDROID_HOME=/usr/local/opt/android-sdk
+brew install infer
