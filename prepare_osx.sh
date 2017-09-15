ae23982e2f32e387a8a2dbe45047844fcfc47a91
Herman Liang <hermanliang80@gmail.com>
2017-09-15 14:02:07 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-COMPONENTS="platform-tools,tools,build-tools-26.0.1,android-26,extra-google-m2repository"
-LICENSES="android-sdk-license-5be876d5|android-sdk-license-c81a61d9"
-curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES && source ~/.android-sdk-installer/env
-
 brew update
 brew install infer
