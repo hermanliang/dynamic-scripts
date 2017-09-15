b5c5e26bfd20a29ba5d6a1406c047eb33571e15d
Herman Liang <hermanliang80@gmail.com>
2017-09-15 18:05:34 +0800

    Update and rename infer_bug_check.sh to infer_analyzer.sh

--- infer_analyzer.sh
+++ infer_analyzer.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-set -e
-
-env
+set -ex
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" || "$TRAVIS_OS_NAME" == "linux" ]]; then
     exit 0
 fi
 
-GRADLE_PATH="$SDK_ROOT/android-studio"
-CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
-COMPARE_BRANCH="develop"
-INFER_OUT="$SDK_ROOT/infer-out"
-
-cd $GRADLE_PATH
-./gradlew clean
-cd $SDK_ROOT
-
 if [ "$CI" == "true" ]; then
+    echo "git fetch origin develop"
+    COMPARE_BRANCH="$TRAVIS_BRANCH"
     CURRENT_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
     git branch $CURRENT_BRANCH
     git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
     git fetch origin $COMPARE_BRANCH
     git checkout $CURRENT_BRANCH
+else
+    SDK_ROOT="$PWD"
+    COMPARE_BRANCH="develop"
+    CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 fi
-echo "Get Diff"
-git diff --name-only origin/develop > index.txt
+INFER_OUT="$SDK_ROOT/infer-out"
+GRADLE_PATH="$SDK_ROOT/android-studio"
+
+cd $GRADLE_PATH
+./gradlew clean
+cd $SDK_ROOT
 echo "Changed files"
+git diff --name-only origin/$COMPARE_BRANCH > index.txt
 cat index.txt
 echo "Analyze branch ${CURRENT_BRANCH}"
 infer capture -- $GRADLE_PATH/gradlew --offline assembleDebug -b $GRADLE_PATH/ce-premium-global/build.gradle
-infer analyze --fail-on-issue -a infer --changed-files-index index.txt
+infer analyze --changed-files-index index.txt
 infer report -q --issues-json report-current.json
 
 echo "Switch to ${COMPARE_BRANCH}"
 git checkout $COMPARE_BRANCH
 echo "Analyze branch ${COMPARE_BRANCH}"
 infer capture --reactive -- $GRADLE_PATH/gradlew --offline assembleDebug -b $GRADLE_PATH/ce-premium-global/build.gradle
-infer analyze --reactive --fail-on-issue -a infer --changed-files-index index.txt
+infer analyze --reactive --changed-files-index index.txt
 infer report -q --issues-json report-compare.json
 echo "Comparing..."
 infer reportdiff --report-current report-current.json --report-previous report-compare.json
@@ -47,9 +47,9 @@ CHANNEL=android_integration
 FILE_PATH=$INFER_OUT/differential/introduced.json
 INTRO_REPORT=$(cat $FILE_PATH)
 INTRO_REPORT_SIZE=${#INTRO_REPORT}
-MESSAGE="$CURRENT_BRANCH Have introduced issues"
 if [[ $INTRO_REPORT_SIZE > "2" ]]; then
-    echo $MESSAGE
+    MESSAGE="$CURRENT_BRANCH has introduced issues. Please check slack channel #android_integration."
+    echo -e "\033[0;31mFailed:\033[1;33m $MESSAGE\033[0m"
     curl \
         -F file=@${FILE_PATH} \
         -F channels=${CHANNEL} \
@@ -58,6 +58,5 @@ if [[ $INTRO_REPORT_SIZE > "2" ]]; then
         -F initial_comment="${MESSAGE}" \
         https://slack.com/api/files.upload
     exit 1
-else
-    echo "No introduced issues"
 fi
+echo "No introduced issues"
