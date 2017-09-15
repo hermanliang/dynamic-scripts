c05b9ab516668219ab5ff96d78027a68892476bf
Herman Liang <hermanliang80@gmail.com>
2017-09-15 13:01:07 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 COMPONENTS="platform-tools,tools,build-tools-26.0.1,android-26,extra-google-m2repository"
 LICENSES="android-sdk-license-5be876d5|android-sdk-license-c81a61d9"
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES && source ~/.android-sdk-installer/env
