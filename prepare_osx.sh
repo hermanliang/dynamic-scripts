dceda8c45dacc95433aed0e9f182e204d0a8a982
Herman Liang <hermanliang80@gmail.com>
2017-09-15 11:44:48 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
+curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=platform-tools,tools,build-tools-26.0.1,android-26,extra-google-m2repository && source ~/.android-sdk-installer/env
+
 brew update
-brew tap caskroom/cask
-brew cask install android-sdk
-eval echo \"y\" | android update sdk --no-ui --force --all --filter extra-google-m2repository || true
-eval echo \"y\" | android update sdk --no-ui --force --all --filter build-tools-26.0.1 || true
-eval echo \"y\" | android update sdk --no-ui --force --all --filter android-26 || true
-export ANDROID_HOME=/usr/local/opt/android-sdk
 brew install infer
