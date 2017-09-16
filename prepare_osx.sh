0cc2be283923758a9d45014f4748d9f3b9c85728
Herman Liang <hermanliang80@gmail.com>
2017-09-16 10:34:29 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-COMPONENTS="platform-tools,tools,build-tools-26.0.1,android-26,extra-google-m2repository"
-LICENSES="android-sdk-license-5be876d5|android-sdk-license-c81a61d9"
-# curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES
-curl -s -L -O https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer
-chmod +x android-sdk-installer
-./android-sdk-installer --install=$COMPONENTS --accept=$LICENSES
-source ~/.android-sdk-installer/env
+if [[ ! -z "$TRAVIS_PULL_REQUEST" && "$TRAVIS_PULL_REQUEST" != "false" && "$TRAVIS_OS_NAME" == "osx" ]]; then
+    COMPONENTS="platform-tools,tools,build-tools-26.0.1,android-26,extra-google-m2repository"
+    LICENSES="android-sdk-license-5be876d5|android-sdk-license-c81a61d9"
+    curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES
+#     curl -s -L -O https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer
+#     chmod +x android-sdk-installer
+#     ./android-sdk-installer --install=$COMPONENTS --accept=$LICENSES
+    source ~/.android-sdk-installer/env
 
-brew update
-brew install infer
+    brew update
+    brew install infer
+fi
