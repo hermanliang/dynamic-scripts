fd5cc1baa5475a96d0af8bb61c4fb9cced482872
Herman Liang <hermanliang80@gmail.com>
2017-09-18 17:23:42 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -13,7 +13,7 @@ if [[ ! -z "$TRAVIS_PULL_REQUEST" && "$TRAVIS_PULL_REQUEST" != "false" && "$TRAV
     git checkout $CURRENT_BRANCH
 
     CHANGED_COUNT=$(git diff --name-only origin/$COMPARE_BRANCH | grep -c -e '.*java')
-    if [[ "$CHANGED_COUNT" -gt "0" ]]; then
+    if [[ $CHANGED_COUNT -gt 0 ]]; then
         COMPONENTS="platform-tools,build-tools-26.0.1,android-26,extra-google-m2repository"
         LICENSES="android-sdk-license-c81a61d9"
         curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES
