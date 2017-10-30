33f35f85baa8caec2bd79273cd78cc973638da34
Herman Liang <hermanliang80@gmail.com>
2017-10-30 11:55:37 +0800

    Update prepare_osx.sh

--- prepare_osx.sh
+++ prepare_osx.sh
@@ -4,7 +4,6 @@ set -e
 
 if [[ ! -z "$TRAVIS_PULL_REQUEST" && "$TRAVIS_PULL_REQUEST" != "false" && "$TRAVIS_OS_NAME" == "osx" ]]; then
 
-    rvm use ruby-2.4.1
     echo "git fetch origin develop"
     COMPARE_BRANCH="$TRAVIS_BRANCH"
     CURRENT_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
@@ -19,6 +18,7 @@ if [[ ! -z "$TRAVIS_PULL_REQUEST" && "$TRAVIS_PULL_REQUEST" != "false" && "$TRAV
         LICENSES="android-sdk-license-c81a61d9|android-sdk-license-2742d1c5"
         curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS --accept=$LICENSES
         source ~/.android-sdk-installer/env
+        brew update
         brew install infer
     else
         echo "No changed java files, ignore configuration"
